cb8aef152f46d58eff7ab93648c2c5853ed9cd7a
Nazmican <nazmican_2133@hotmail.com>
2021-05-23 23:23:33 +0300

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "./App.css";
+import { Container } from "semantic-ui-react";
+import SubmitForm from "./submit-form";
+
+function App() {
+  return (
+    <div>
+      <Container>
+        <SubmitForm />
+      </Container>
+    </div>
+  );
+}
+
+export default App;
